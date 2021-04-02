d724041a7de49bfd7ca73a2cfe13fe57f44fe1e8
dinogrgic1 <dino.grgic1@gmail.com>
2021-04-02 16:30:26 +0200

    Added padding

--- lab1/pwd_manager.py
+++ lab1/pwd_manager.py
@@ -3,7 +3,9 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA512
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 
+# TODO(Dino): Check if key size of 32 is big enough
 PBKDF2_KEY_SIZE = 32
 PBKDF2_ITTERATIONS = 1000000
 PBKDF2_SALT_SIZE = 16
@@ -18,6 +20,7 @@ VAULT_EMPTY = '==vault=='
 VAULT_HEADER_SIZE = PBKDF2_SALT_SIZE + AES_TAG_SIZE + AES_NONCE_SIZE
 VAULT_ENTRY_SEPARATOR = '\n'
 VAULT_ADDRESS_SEPARATOR = '\t'
+VAULT_PADDING_SIZE = 256
 
 
 def AES_encrypt(key, plaintext):
@@ -26,8 +29,9 @@ def AES_encrypt(key, plaintext):
     if type(plaintext) is bytes:
         ciphertext, tag = cipher.encrypt_and_digest(plaintext)
     else:
-        ciphertext, tag = cipher.encrypt_and_digest(plaintext.encode('utf-8'))
+        ciphertext, tag = cipher.encrypt_and_digest(plaintext.encode('ascii'))
     return (ciphertext, nonce, tag)
+
 
 def AES_decrypt(key, ciphertext, nonce, tag):
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
@@ -37,6 +41,7 @@ def AES_decrypt(key, ciphertext, nonce, tag):
         return plaintext
     except ValueError:
         raise Exception("Master password is incorrect or vault is corrupted.")
+
 
 def key_password_derivation(master_pass=None, salt=get_random_bytes(PBKDF2_SALT_SIZE)):
     if master_pass is None:
@@ -52,8 +57,8 @@ def vault_init():
     vault_file.write(salt)
 
     vault, nonce, tag = AES_encrypt(key, VAULT_EMPTY)
-    vault_file.write(nonce) 
-    vault_file.write(tag) 
+    vault_file.write(nonce)
+    vault_file.write(tag)
     vault_file.write(vault)
     vault_file.close()
 
@@ -62,6 +67,7 @@ def vault_put_password(master_password, address):
     vault_file = open(VAULT_PATH, 'r+b')
     password = input(f"Password for {address} will be: ")
 
+    # TODO(Dino): Don't read the whole file in memory when doing I/O?
     vault_file_content = vault_file.read()
     vault_header = vault_file_content[:VAULT_HEADER_SIZE]
     vault_body = vault_file_content[VAULT_HEADER_SIZE:]
@@ -71,11 +77,19 @@ def vault_put_password(master_password, address):
     tag = vault_header[AES_TAG_POSITION:]
 
     key, salt = key_password_derivation(master_password, salt)
-
     vault_body = AES_decrypt(key, vault_body, nonce, tag)
-    if vault_body == VAULT_EMPTY.encode('utf-8'):
+
+    # TODO(Dino): Don't add password if it exists already
+    if vault_body == VAULT_EMPTY.encode('ascii'):
         vault_body = b''
-    vault_body += f'{address}{VAULT_ADDRESS_SEPARATOR}{password}{VAULT_ENTRY_SEPARATOR}'.encode('utf-8')
+
+    pad_address = pad(bytes(address , encoding=('ascii')), VAULT_PADDING_SIZE)
+    pad_password = pad(bytes(password , encoding=('ascii')), VAULT_PADDING_SIZE)
+
+    vault_body += pad_address
+    vault_body += VAULT_ADDRESS_SEPARATOR.encode('ascii')
+    vault_body += pad_password
+    vault_body += VAULT_ENTRY_SEPARATOR.encode('ascii')
 
     ciphertext, nonce, tag = AES_encrypt(key, vault_body)
     vault_file.truncate(0)
@@ -84,6 +98,7 @@ def vault_put_password(master_password, address):
     vault_file.write(nonce)
     vault_file.write(tag)
     vault_file.write(ciphertext)
+
 
 def vault_get_password(master_password, address):
     vault_file = open(VAULT_PATH, 'rb')
@@ -99,14 +114,18 @@ def vault_get_password(master_password, address):
     key, salt = key_password_derivation(master_password, salt)
     vault_body = AES_decrypt(key, vault_body, nonce, tag)
 
-    entries = vault_body.decode('utf-8').split(VAULT_ENTRY_SEPARATOR)
+    entries = vault_body.decode('ascii', 'ignore').split(VAULT_ENTRY_SEPARATOR)
     for entry in entries:
+        if len(entry) < 1:
+            continue
         pair = entry.split(VAULT_ADDRESS_SEPARATOR)
         if pair[0] == address:
-            return pair
+            return (pair[0], pair[1])
     raise Exception('Adress not found in the vault.')
 
+
 def parse_args(args):
+    # TODO(Dino): Check if password or address is longer then 256 characters
     try:
         if args[1] == 'init':
             vault_init()
